78f653c038737cf3ac2236377ffffa5d2a398134
Luis <luisadolfo.13@outlook.com>
2022-06-12 20:14:18 -0400

    Alteração no endereço do cliente

--- src/models/Cliente.js
+++ src/models/Cliente.js
@@ -2,46 +2,68 @@ const mongoose = require("mongoose")
 const { Schema } = mongoose
 
 const clienteSchema = new Schema({
-  nome: {
-    type: String,
-    required: true
-  },
-  cpf: {
-    type: String,
-    required: true
-  },
-  dataNascimento: {
-    type: Date,
-    required: true
-  },
-  endereco: {
-    type: String,
-    required: true
-  },
-  usuario: {
-    email: {
-      type: String,
-      required: true,
-      unique: true
+    nome: {
+        type: String,
+        required: true
     },
-    senha: {
-      type: String,
-      required: true,
-      select: false
-    }
-  },
-  compras: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Compra"
-    }
-  ],
-  favoritos: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Produto"
-    }
-  ]
+    cpf: {
+        type: String,
+        required: true
+    },
+    dataNascimento: {
+        type: Date,
+        required: true
+    },
+    endereco: {
+        bairro: {
+            type: String,
+            required: true
+        },
+        cep: {
+            type: String,
+            required: true
+        },
+        complemento: {
+            type: String,
+            required: false
+        },
+        localidade: {
+            type: String,
+            required: true
+        },
+        logradouro: {
+            type: String,
+            required: true
+        },
+        numero: {
+            type: Number,
+            required: true
+        },
+        uf: {
+            type: String,
+            required: true
+        }
+    },
+    usuario: {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        senha: {
+            type: String,
+            required: true,
+            select: false
+        }
+    },
+    compras: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Compra"
+    }],
+    favoritos: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Produto"
+    }]
 })
 
 module.exports = mongoose.model("Cliente", clienteSchema)
